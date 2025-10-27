a36c0ae89c6792fe18ed865eebf99aeba8899c40
Devanshu Upadhyay <30511315+devanshuupadhyay@users.noreply.github.com>
2025-10-27 00:31:43 -0400

    feat: Updated readme with website info

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,27 +1,31 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   env: {
-    CYPRESS_RUNNING: 'true'
+    CYPRESS_RUNNING: "true",
   },
   e2e: {
     viewportWidth: 1920,
     viewportHeight: 1080,
-    video: true, 
+    video: true,
     videoCompression: false,
-    baseUrl: 'http://localhost:3000',
-    chromeWebSecurity: false, 
+    baseUrl: "http://localhost:3000",
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser, launchOptions) => {
+      on("before:browser:launch", (browser, launchOptions) => {
         // This targets Chrome and other Chromium-based browsers
-        if (browser.family === 'chromium') {
-          launchOptions.preferences.default['credentials_enable_service'] = false
-          launchOptions.preferences.default['profile.password_manager_enabled'] =
-            false
+        viewportWidth: 1920;
+        viewportHeight: 1080;
+        if (browser.family === "chromium") {
+          launchOptions.preferences.default["credentials_enable_service"] =
+            false;
+          launchOptions.preferences.default[
+            "profile.password_manager_enabled"
+          ] = false;
         }
 
-        return launchOptions
-      })
+        return launchOptions;
+      });
     },
   },
-})
+});
